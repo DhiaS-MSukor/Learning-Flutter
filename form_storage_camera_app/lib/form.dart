a040d3d6105b690706a2e3352970d1ed7272f12a
DhiaSyahmie <D.MuhamadSukor001@umb.edu>
2020-08-10 13:15:47 +0800

    added get image functionalities

--- form_storage_camera_app/lib/form.dart
+++ form_storage_camera_app/lib/form.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FormWidget extends StatefulWidget {
   @override
@@ -7,6 +11,8 @@ class FormWidget extends StatefulWidget {
 
 class _FormWidgetState extends State<FormWidget> {
   final _formKey = GlobalKey<FormState>();
+  PickedFile _imageFile;
+  ImagePicker _picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +27,51 @@ class _FormWidgetState extends State<FormWidget> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Text'),
               ),
+              SizedBox(height: 100),
+              RaisedButton(
+                onPressed: () {
+                  _selectImageButton(context);
+                },
+                child: _imageFile == null
+                    ? Icon(Icons.image)
+                    : Image.file(File(_imageFile.path)),
+              ),
               RaisedButton(onPressed: null, child: Text('Submit')),
             ],
           ),
         ));
+  }
+
+  Future<void> _selectImageButton(context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Choose method'),
+            content: Column(children: [
+              RaisedButton(
+                  onPressed: () {
+                    _pickImage(ImageSource.camera);
+                    Navigator.of(context).pop();
+                  },
+                  child: Icon(Icons.camera_alt)),
+              RaisedButton(
+                onPressed: () {
+                  _pickImage(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+                child: Icon(Icons.photo_library),
+              )
+            ]),
+          );
+        });
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await _picker.getImage(source: source);
+
+    setState(() {
+      _imageFile = pickedFile;
+    });
   }
 }
